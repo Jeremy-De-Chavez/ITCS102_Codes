9b68e23281250d5664da0cf8038c77fa4676b0e5
Jeremy-De-Chavez <dechavez791@gmail.com>
2026-09-01 10:57:30 +0800

    Add files via upload

---
+++ code_challenge_2.py
@@ -0,0 +1,52 @@
+money = int(input("enter amount to deposit -"))
+
+money2 = money//1000
+money = money % 1000
+print("1000 =", money2)
+
+money3 = money//500
+money = money % 500
+print("500 =", money3)
+
+money4 = money//200
+money = money % 200
+print("200 =", money4)
+
+money5 = money//100
+money = money % 100
+print("100 =", money5)
+
+money6 = money//50
+money = money % 50
+print("50 =", money6)
+
+money7 = money//20
+money = money % 20
+print("20 =", money7)
+
+money8 = money//10
+money = money % 10
+print("10 =", money8)
+
+money9 = money//5
+money = money % 5
+print("5 =", money9)
+
+money10 = money//1
+money = money % 1
+print("1 =", money10)
+
+money = money % 1000
+money = money % 500
+money = money % 200
+money = money % 100
+money = money % 50
+money = money % 20
+money = money % 10
+money = money % 5
+money = money % 1
+
+
+
+
+
